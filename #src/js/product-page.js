c893ea9319ae65217e2af265a822f82d6097ca5b
life-f <58433392+life-f@users.noreply.github.com>
2021-06-18 22:36:23 +0300

    add desktop and tablet adaptive

--- #src/js/product-page.js
+++ #src/js/product-page.js
@@ -1,31 +1,32 @@
 $(document).ready(function () {
-    // if ($(window).width() >= '1440') {
-    $('.product__gallery-open').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-        prevArrow: '<div class="arrow-prev arrow"><img src="../img/product-page/prev.svg"></div>',
-        nextArrow: '<div class="arrow-next arrow"><img src="../img/product-page/next.svg"></div>',
-        fade: true,
-        asNavFor: '.product__gallery-carousel'
-    });
-    $('.product__gallery-carousel').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        centerMode: true,
-        focusOnSelect: true,
-        infinite: true,
-        asNavFor: '.product__gallery-open'
-    });
-    // } else if ($(window).width() >= '1024') {
-    //     $('#slider-1').slick({
-    //         slidesToShow: 2.14,
-    //         variableWidth: false,
-    //         arrows: false,
-    //         dots: true,
-    //         infinite: false
-    //     });
-    // } else if ($(window).width() >= '768') {
+    if ($(window).width() >= '1440') {
+        $('.product__gallery-open').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: true,
+            prevArrow: '<div class="arrow-prev arrow"><img src="../img/product-page/prev.svg"></div>',
+            nextArrow: '<div class="arrow-next arrow"><img src="../img/product-page/next.svg"></div>',
+            fade: true,
+            asNavFor: '.product__gallery-carousel'
+        });
+        $('.product__gallery-carousel').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            centerMode: true,
+            focusOnSelect: true,
+            infinite: true,
+            asNavFor: '.product__gallery-open'
+        });
+    } else if ($(window).width() >= '1024') {
+        $('.product__gallery-carousel').slick({
+            slidesToShow: 2.3,
+            slidesToScroll: 1,
+            variableWidth: false,
+            dots: true,
+            infinite: false,
+        });
+    }
+    // else if ($(window).width() >= '768') {
     //     $('#slider-1').slick({
     //         slidesToShow: 1.6,
     //         variableWidth: false,
