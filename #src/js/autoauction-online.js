5a3765cac305e069750940040cdadbb3ad0c9a6d
life-f <58433392+life-f@users.noreply.github.com>
2021-06-20 21:27:48 +0300

    add filter-adaptive

--- #src/js/autoauction-online.js
+++ #src/js/autoauction-online.js
@@ -4,7 +4,6 @@ $(document).ready(function () {
     for (let i = 0; i < lists.length; i++) {
         if (lists[i].classList.contains('active')) {
             selectedList = lists[i]
-            console.log(i)
         }
     }
 
@@ -14,4 +13,22 @@ $(document).ready(function () {
         selectedList = lists[filterSelect.selectedIndex]
         selectedList.classList.add("active")
     };
+
+    $('#filter-btn').on("click", function () {
+        console.log('open')
+        $('.main-filter__filter').addClass('active')
+        $('.main-filter__filter-mobile').addClass('not-visible')
+    });
+    $('#filter__close-btn').on("click", function () {
+        $('.main-filter__filter').removeClass('active')
+        $('.main-filter__filter-mobile').removeClass('not-visible')
+    });
+    $('.clear-btn').on("click", function () {
+        $('.main-filter__filter').removeClass('active')
+        $('.main-filter__filter-mobile').removeClass('not-visible')
+    });
+    $('.show-btn').on("click", function () {
+        $('.main-filter__filter').removeClass('active')
+        $('.main-filter__filter-mobile').removeClass('not-visible')
+    });
 })
