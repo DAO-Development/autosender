bb26782870166cfef1059a4305434313468e6448
life-f <58433392+life-f@users.noreply.github.com>
2021-06-21 15:03:56 +0300

    add youtube-slider

--- #src/js/autoauction-online.js
+++ #src/js/autoauction-online.js
@@ -31,4 +31,63 @@ $(document).ready(function () {
         $('.main-filter__filter').removeClass('active')
         $('.main-filter__filter-mobile').removeClass('not-visible')
     });
+
+    if ($(window).width() >= '1440') {
+        $('.youtube-slider').slick({
+            slidesToShow: 3,
+            variableWidth: false,
+            arrows: true,
+            prevArrow: '<div class="arrow-prev arrow"><img src="../img/autoauction-online/prev-arrow.svg"></div>',
+            nextArrow: '<div class="arrow-next arrow"><img src="../img/autoauction-online/next-arrow.svg"></div>',
+            infinite: false
+        });
+    } else if ($(window).width() >= '1024') {
+        $('.youtube-slider').slick({
+            slidesToShow: 2.2,
+            variableWidth: false,
+            arrows: false,
+            dots: true,
+            infinite: false
+        });
+    } else if ($(window).width() >= '768') {
+        $('.youtube-slider').slick({
+            slidesToShow: 1.6,
+            variableWidth: false,
+            arrows: false,
+            dots: true,
+            infinite: false
+        });
+    } else if ($(window).width() >= '568') {
+        $('.youtube-slider').slick({
+            slidesToShow: 1.4,
+            variableWidth: false,
+            arrows: false,
+            dots: true,
+            infinite: false
+        });
+    } else if ($(window).width() >= '414') {
+        $('.youtube-slider').slick({
+            slidesToShow: 1.2,
+            variableWidth: false,
+            arrows: false,
+            dots: true,
+            infinite: false
+        });
+    } else if ($(window).width() >= '360') {
+        $('.youtube-slider').slick({
+            slidesToShow: 1.1,
+            variableWidth: false,
+            arrows: false,
+            dots: true,
+            infinite: false
+        });
+    } else if ($(window).width() < '360') {
+        $('.youtube-slider').slick({
+            slidesToShow: 1,
+            variableWidth: false,
+            arrows: false,
+            dots: true,
+            infinite: false
+        });
+    }
 })
