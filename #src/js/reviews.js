3a68f81fd7fccdb287fe50004394f6d1b41fffb6
life-f <58433392+life-f@users.noreply.github.com>
2021-06-21 21:04:33 +0300

    fix rewievs-slider

--- #src/js/reviews.js
+++ #src/js/reviews.js
@@ -1,24 +1,71 @@
 $(document).ready(function () {
-    console.log("pitty");
-    $('.reviews__items').slick({
-        slidesToShow: 3,
-        variableWidth: false,
-        arrows: false,
-        dots: true,
-        infinite: false,
-        responsive: [
-            {
-              breakpoint: 1400,
-              settings: {
-                slidesToShow: 2,
-              }
-            },
-            {
-              breakpoint: 570,
-              settings: {
-                slidesToShow: 1,
-              }
-            },
-        ]
-    });
+    if ($(window).width() >= '1440') {
+        $('.reviews__items').slick({
+            slidesToShow: 3,
+            variableWidth: false,
+            arrows: false,
+            dots: true,
+            infinite: false,
+            // responsive: [
+            //     {
+            //         breakpoint: 1440,
+            //         settings: {
+            //             slidesToShow: 2,
+            //         }
+            //     },
+            //     {
+            //         breakpoint: 1024,
+            //         settings: {
+            //             slidesToShow: 2.2,
+            //         }
+            //     },
+            //     {
+            //         breakpoint: 768,
+            //         settings: {
+            //             slidesToShow: 1.6,
+            //         }
+            //     },
+            // ]
+        });
+    } else if ($(window).width() >= '1024') {
+        $('.reviews__items').slick({
+            slidesToShow: 2,
+            variableWidth: false,
+            arrows: false,
+            dots: true,
+            infinite: false,
+        });
+    } else if ($(window).width() >= '768') {
+        $('.reviews__items').slick({
+            slidesToShow: 2.2,
+            variableWidth: false,
+            arrows: false,
+            dots: true,
+            infinite: false
+        });
+    } else if ($(window).width() >= '568') {
+        $('.reviews__items').slick({
+            slidesToShow: 1.5,
+            variableWidth: false,
+            arrows: false,
+            dots: true,
+            infinite: false
+        });
+    } else if ($(window).width() >= '360') {
+        $('.reviews__items').slick({
+            slidesToShow: 1.2,
+            variableWidth: false,
+            arrows: false,
+            dots: true,
+            infinite: false
+        });
+    } else if ($(window).width() < '360') {
+        $('.reviews__items').slick({
+            slidesToShow: 1,
+            variableWidth: false,
+            arrows: false,
+            dots: true,
+            infinite: false
+        });
+    }
 });
