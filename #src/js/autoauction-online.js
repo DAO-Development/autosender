33c362ea9918f95e79fa22b0458f7eb5f90ca7a2
life-f <58433392+life-f@users.noreply.github.com>
2021-06-23 15:01:51 +0300

    fix filter

--- #src/js/autoauction-online.js
+++ #src/js/autoauction-online.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
+    if ($(window).width() >= '1024') {
+        $('.filter__color').addClass('not-visible')
+        $('.filter__auctions').addClass('not-visible')
+        $('.filter__num-lot').addClass('not-visible')
+        $('.filter__body-model').addClass('not-visible')
+        $('.filter__body-type').addClass('not-visible')
+        $('.filter__mileage').addClass('not-visible')
+        $('.filter__volume').addClass('not-visible')
+        $('.filter__drive').addClass('not-visible')
+        $('.filter__num-body').addClass('not-visible')
+    }
     let lists = document.getElementsByClassName('main-filter__list-category')
     let selectedList;
     for (let i = 0; i < lists.length; i++) {
@@ -18,6 +29,32 @@ $(document).ready(function () {
         console.log('open')
         $('.main-filter__filter').addClass('active')
         $('.main-filter__filter-mobile').addClass('not-visible')
+    });
+    $('.all-btn').on("click", function () {
+        let btn = document.querySelector('.all-btn-svg')
+        if (btn.classList.contains('closed')) {
+            btn.classList.remove('closed')
+            $('.filter__color').removeClass('not-visible')
+            $('.filter__auctions').removeClass('not-visible')
+            $('.filter__num-lot').removeClass('not-visible')
+            $('.filter__body-model').removeClass('not-visible')
+            $('.filter__body-type').removeClass('not-visible')
+            $('.filter__mileage').removeClass('not-visible')
+            $('.filter__volume').removeClass('not-visible')
+            $('.filter__drive').removeClass('not-visible')
+            $('.filter__num-body').removeClass('not-visible')
+        } else {
+            $('.all-btn-svg').addClass('closed')
+            $('.filter__color').addClass('not-visible')
+            $('.filter__auctions').addClass('not-visible')
+            $('.filter__num-lot').addClass('not-visible')
+            $('.filter__body-model').addClass('not-visible')
+            $('.filter__body-type').addClass('not-visible')
+            $('.filter__mileage').addClass('not-visible')
+            $('.filter__volume').addClass('not-visible')
+            $('.filter__drive').addClass('not-visible')
+            $('.filter__num-body').addClass('not-visible')
+        }
     });
     $('#filter__close-btn').on("click", function () {
         $('.main-filter__filter').removeClass('active')
