a2454005bf2b8122d3b1342a2a306a838a2268a9
Sascha <sasha.bubnov.k@gmail.com>
2022-05-06 17:16:01 +0300

    add acc-mob for full price

--- #src/js/product-page.js
+++ #src/js/product-page.js
@@ -111,4 +111,11 @@ $(document).ready(function () {
         else
             $('.product__acc[data-id="'+data_id+'"]').addClass('show')
     })
+    $('.product__acc-mobile-title').on('click', function () {
+        let data_id = $(this).attr('data-id')
+        if ($('.product__acc-mobile[data-id="'+data_id+'"]').hasClass('show'))
+            $('.product__acc-mobile[data-id="'+data_id+'"]').removeClass('show')
+        else
+            $('.product__acc-mobile[data-id="'+data_id+'"]').addClass('show')
+    })
 });
