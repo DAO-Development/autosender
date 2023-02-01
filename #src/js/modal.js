0b6fad76528a928124eff2630e450d03ba047286
Sascha <sasha.bubnov.k@gmail.com>
2023-02-01 08:01:31 +0300

    modal-video, fix youtube, reviews it index

--- #src/js/modal.js
+++ #src/js/modal.js
@@ -1,8 +1,21 @@
 $('.open-modal').on("click", function(){
     $('.modal').addClass('vis')
 })
-
-$('.modal__close').on("click", function(){
+ 	
+$('.modal, .modal__close').on("click", function(){
     $('.modal').removeClass('vis')
+    $('.modal iframe').attr('src', $('iframe').attr('src'));
+
 })
 
+
+$( ".modal__wrap" ).click(function( event ) {
+    event.stopPropagation();
+  });
+$('.video-reviews-item').on('click', function (e) {
+    e.preventDefault();
+    let code = $(this).attr('href').split('watch?v=')[1]
+    $('.modal-video').addClass('vis'); 
+    $('.modal-video iframe').attr('src', 'https://www.youtube.com/embed/' + code)
+    
+})
